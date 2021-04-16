35a475e07b37276a43bb0084a9a35b4076978db6
tachibana687 <wsamkprimeape@gmail.com>
2021-04-16 11:35:09 +0900

    [SIF-1] ナビゲーションバーの実装

---
+++ lib/page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class CustomPage extends StatelessWidget {
+  final Color pannelColor;
+  final String title;
+
+  CustomPage({@required this.pannelColor, @required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    final titleTextStyle = Theme.of(context).textTheme.title;
+    return Container(
+      child: Center(
+        child: Container(
+          decoration: BoxDecoration(
+            color: pannelColor,
+          ),
+          child: Center(
+            child: Text(
+              title,
+              style: TextStyle(
+                fontSize: titleTextStyle.fontSize,
+                color: titleTextStyle.color,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
