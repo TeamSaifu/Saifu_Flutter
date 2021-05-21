fac50d2202d5413e244832b0b246dd9be9b1c187
Nakafuji Mizuto <2180417@ecccomp.com>
2021-05-21 13:46:10 +0900

    通知機能を作成しました。

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,51 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:saifu/page.dart';
-import 'package:saifu/wish_list.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:saifu/add_wish_item.dart';
 import 'package:saifu/log.dart';
+import 'package:saifu/page.dart';
+import 'package:saifu/wish_list.dart';
 
-void main() {
+// バックグラウンドで通知を使用するやつ
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print("Handling a background message ${message.messageId}");
+}
+
+// 通知用のチャンネルを作成
+const AndroidNotificationChannel channel = AndroidNotificationChannel(
+  'high_importance_channel', // id
+  'High Importance Notifications', // title
+  'This channel is used for important notifications.', // description
+  importance: Importance.high,
+  enableVibration: true,
+  playSound: true,
+);
+
+// 通知用のパッケージ導入
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  //名前付きのトップレベル関数として設定
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  //FCMチャンネルをオーバーライドし通知を有効にする
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+  // iOSのフォアグラウンド通知の表示オプションを更新する
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+  await DotEnv().load('.env');
   runApp(MyApp());
 }
 
@@ -32,9 +73,10 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  MyHomePage({Key key, this.title, this.message}) : super(key: key);
 
   final String title;
+  final RemoteMessage message;
 
   // ナビゲーションバー
   // https://dev.classmethod.jp/articles/basic_bottom_navigation_flutter/
@@ -46,7 +88,40 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _selectedIndex = 0;
 
-  //画面のリストを作成 (ここで切り替え先を指定する）
+  // 通知のあれこれ
+  @override
+  void initState() {
+    FirebaseMessaging.instance
+        .getInitialMessage()
+        .then((RemoteMessage message) async {
+      if (message != null) {
+        print(message);
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => MyHomePage(message: message),
+          ),
+        );
+      }
+      // Logcatにtoken表示
+      String token = await FirebaseMessaging.instance.getToken();
+      print("token:" + token);
+    });
+
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) async {
+      print('A new onMessageOpenedApp event was published!');
+      print(message);
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => MyHomePage(message: message),
+        ),
+      );
+    });
+    super.initState();
+  }
+
+//画面のリストを作成 (ここで切り替え先を指定する）
   static List<Widget> _pageList = [
     CustomPage(pannelColor: Colors.cyan, title: 'ホーム'),
     WishListPage(),
@@ -54,7 +129,7 @@ class _MyHomePageState extends State<MyHomePage> {
     CustomPage(pannelColor: Colors.cyan, title: '設定'),
   ];
 
-  // 一番上のタイトルを変えるとこ
+// 一番上のタイトルを変えるとこ
   static List<Widget> _pageTitle = [
     Text('ホーム'),
     Text('欲しい物リスト'),
@@ -68,7 +143,7 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  //本体
+//本体
   @override
   Widget build(BuildContext context) {
     return Scaffold(
