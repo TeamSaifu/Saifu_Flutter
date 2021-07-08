22f0d44e414c332fdea2a92999304a1d2b99040b
tachibana687 <wsamkprimeape@gmail.com>
2021-07-08 15:39:04 +0900

    ショートカット編集画面に遷移するようにした

--- lib/setting.dart
+++ lib/setting.dart
@@ -10,61 +10,65 @@ class Setting extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-            child: Column(
-      children: [
-        SwitchListTile(
-          title: Text('通知'),
-          value: _toggled,
-          secondary: Icon(Icons.airplanemode_active),
-          onChanged: (bool value) {},
-        ),
-        Divider(
-          thickness: 1.2,
-        ),
-        ListTile(
-          leading: Icon(Icons.monetization_on),
-          title: Text("予算入力"),
-          onTap: () {},
-        ),
-        Divider(
-          thickness: 1.2,
-        ),
-        ListTile(
-          leading: Icon(Icons.folder_open),
-          title: Text("カテゴリ一覧"),
-          onTap: () {},
-        ),
-        Divider(
-          thickness: 1.2,
-        ),
-        SwitchListTile(
-          title: Text("起動時に入力画面に推移"),
-          value: _toggled,
-          onChanged: (bool value) {},
-          secondary: const Icon(Icons.fast_forward),
-        ),
-        Divider(
-          thickness: 1.2,
-        ),
-        SwitchListTile(
-          title: Text("ショートカット即時登録"),
-          value: _toggled,
-          onChanged: (bool value) {},
-          secondary: const Icon(Icons.fast_forward),
-        ),
-        Divider(
-          thickness: 1.2,
-        ),
-        ListTile(
-          leading: Icon(Icons.edit),
-          title: Text("ショートカット編集"),
-          onTap: () {},
-        ),
-        Divider(
-          thickness: 1.2,
-        ),
-      ],
-    )));
+      body: Center(
+        child: Column(
+          children: [
+            SwitchListTile(
+              title: Text('通知'),
+              value: _toggled,
+              secondary: Icon(Icons.airplanemode_active),
+              onChanged: (bool value) {},
+            ),
+            Divider(
+              thickness: 1.2,
+            ),
+            ListTile(
+              leading: Icon(Icons.monetization_on),
+              title: Text("予算入力"),
+              onTap: () {},
+            ),
+            Divider(
+              thickness: 1.2,
+            ),
+            ListTile(
+              leading: Icon(Icons.folder_open),
+              title: Text("カテゴリ一覧"),
+              onTap: () {},
+            ),
+            Divider(
+              thickness: 1.2,
+            ),
+            SwitchListTile(
+              title: Text("起動時に入力画面に推移"),
+              value: _toggled,
+              onChanged: (bool value) {},
+              secondary: const Icon(Icons.fast_forward),
+            ),
+            Divider(
+              thickness: 1.2,
+            ),
+            SwitchListTile(
+              title: Text("ショートカット即時登録"),
+              value: _toggled,
+              onChanged: (bool value) {},
+              secondary: const Icon(Icons.fast_forward),
+            ),
+            Divider(
+              thickness: 1.2,
+            ),
+            ListTile(
+              leading: Icon(Icons.edit),
+              title: Text("ショートカット編集"),
+              onTap: () {
+                Navigator.of(context).pushNamed('/shortcut');
+              },
+            ),
+            Divider(
+              thickness: 1.2,
+            ),
+          ],
+        )
+      )
+    );
   }
 }
