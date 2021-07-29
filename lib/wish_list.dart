c79f1a9967aff4bdf413e488ec765384b0361885
tachibana687 <wsamkprimeape@gmail.com>
2021-07-29 09:41:36 +0900

    いったんコミット

--- lib/wish_list.dart
+++ lib/wish_list.dart
@@ -17,7 +17,7 @@ class _WishListPageState extends State<WishListPage> {
   final formatter = NumberFormat("#,###");
 
   // WishItemの一覧リスト変数を用意
-  List<WishItemModel> wishItemList = [
+  List<WishItemModel> _wishItemList = [
     WishItemModel(item: 'サメマゲドン', price: 3300),
     WishItemModel(item: '単位', price: 10000),
   ];
@@ -27,9 +27,9 @@ class _WishListPageState extends State<WishListPage> {
     return Scaffold(
       body: ListView.builder(
         // リストの長さを計算
-        itemCount: wishItemList.length,
+        itemCount: _wishItemList.length,
         itemBuilder: (BuildContext context, int index) {
-          return _wishListItem(index);
+          return WishListItem(item: _wishItemList[index].item, price: _wishItemList[index].price);
         },
       ),
       floatingActionButton: FloatingActionButton(
@@ -39,63 +39,153 @@ class _WishListPageState extends State<WishListPage> {
           final result = await Navigator.of(context).pushNamed('/add');
           if (result != null) {
             setState(() {
-              wishItemList.add(result);
+              _wishItemList.add(result);
             });
           }
         },
       ),
     );
   }
+}
 
-  // 欲しい物リストのアイテム
-  Widget _wishListItem(index) {
-    return Container(
+// 欲しい物リストのアイテム
+class WishListItem extends StatefulWidget {
+  WishListItem({Key key, @required this.item, @required this.price}) : super(key: key);
+
+  final String item;
+  final int price;
+
+  @override
+  _WishListItemState createState() => _WishListItemState();
+}
+
+class _WishListItemState extends State<WishListItem> {
+  final formatter = NumberFormat("#,###");
+
+  bool _appearButtons = false;
+
+  Widget _functionButton({@required IconData icon}) {
+    return SizedBox(
+      width: 70,
       height: 70,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          primary: Colors.red.withOpacity(0.0),
+          shadowColor: Colors.red.withOpacity(0.0),
+        ),
+        child: Icon(icon, color: Colors.grey.shade600, size: 40,),
+        onPressed: () {
+
+        },
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
       // Itemのボーダー線
       decoration: const BoxDecoration(
           border: const Border(bottom: BorderSide(width: 1.0, color: Colors.grey))
       ),
-      child: Row(
+      child: Column(
         children: [
-          Expanded(
-            flex: 3,
-            child: Text(
-              wishItemList[index].item,
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                  color: Colors.grey.shade700,
-                  fontSize: 20.0
+          Row(
+            children: [
+              Expanded(
+                flex: 3,
+                child: Text(
+                  widget.item,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Colors.grey.shade700,
+                      fontSize: 20.0
+                  ),
+                ),
               ),
-            ),
-          ),
-          Expanded(
-            flex: 2,
-            child: Text(
-              '￥' + formatter.format(wishItemList[index].price),
-              style: TextStyle(
-                  color: Colors.grey.shade600,
-                  fontSize: 22.0
+              Expanded(
+                flex: 2,
+                child: Text(
+                  '￥' + formatter.format(widget.price),
+                  style: TextStyle(
+                      color: Colors.grey.shade600,
+                      fontSize: 22.0
+                  ),
+                ),
               ),
-            ),
-          ),
-          SizedBox(
-            // SizedBoxでボタンのサイズを指定
-            width: 70,
-            height: 70,
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                // ボタンの背景色を透明化
-                primary: Colors.red.withOpacity(0.0),
-                shadowColor: Colors.red.withOpacity(0.0),
+              SizedBox(
+                // SizedBoxでボタンのサイズを指定
+                width: 70,
+                height: 70,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    // ボタンの背景色を透明化
+                    primary: Colors.red.withOpacity(0.0),
+                    shadowColor: Colors.red.withOpacity(0.0),
+                  ),
+                  child: Icon(Icons.arrow_drop_down, color: Colors.grey.shade600, size: 40,),
+                  onPressed: () {
+                    setState(() {
+                      _appearButtons = !_appearButtons;
+                    });
+                  },
+                ),
               ),
-              child: Icon(Icons.arrow_drop_down, color: Colors.grey.shade600, size: 40,),
-              onPressed: () {
-                // ボタンの処理
-              },
-            ),
+            ],
           ),
+          _appearButtons? Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              _functionButton(icon: Icons.home),
+              _functionButton(icon: Icons.settings),
+              _functionButton(icon: Icons.work),
+            ],
+          ) : Container(),
         ],
       ),
+      // child: Row(
+      //   children: [
+      //     Expanded(
+      //       flex: 3,
+      //       child: Text(
+      //         widget.item,
+      //         textAlign: TextAlign.center,
+      //         style: TextStyle(
+      //             color: Colors.grey.shade700,
+      //             fontSize: 20.0
+      //         ),
+      //       ),
+      //     ),
+      //     Expanded(
+      //       flex: 2,
+      //       child: Text(
+      //         '￥' + formatter.format(widget.price),
+      //         style: TextStyle(
+      //             color: Colors.grey.shade600,
+      //             fontSize: 22.0
+      //         ),
+      //       ),
+      //     ),
+      //     SizedBox(
+      //       // SizedBoxでボタンのサイズを指定
+      //       width: 70,
+      //       height: 70,
+      //       child: ElevatedButton(
+      //         style: ElevatedButton.styleFrom(
+      //           // ボタンの背景色を透明化
+      //           primary: Colors.red.withOpacity(0.0),
+      //           shadowColor: Colors.red.withOpacity(0.0),
+      //         ),
+      //         child: Icon(Icons.arrow_drop_down, color: Colors.grey.shade600, size: 40,),
+      //         onPressed: () {
+      //           setState(() {
+      //             _appearButtons = !_appearButtons;
+      //           });
+      //         },
+      //       ),
+      //     ),
+      //   ],
+      // ),
     );
   }
 }
