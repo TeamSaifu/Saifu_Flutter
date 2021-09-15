c2b1e9ca350042cd4834dbabdb64b228b2c569ca
tachibana687 <wsamkprimeape@gmail.com>
2021-09-15 20:02:47 +0900

    データベース作成

--- lib/db/db_provider.dart
+++ lib/db/db_provider.dart
@@ -29,8 +29,9 @@ class DBProvider {
       path,
       version: _databaseVersion,
       onCreate: (db, version) async {
+        print('ok');
         var batch = db.batch();
-        _createTable(batch);
+        await _createTable(batch);
         await batch.commit();
       },
       onDowngrade: onDatabaseDowngradeDelete,
@@ -38,13 +39,62 @@ class DBProvider {
   }
 
   void _createTable(Batch batch) {
-    batch.execute(
-        '''CREATE TABLE input_text(
+    batch.execute('''
+      CREATE TABLE category(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        body TEXT NOT NULL,
-        created_at TEXT NOT NULL,
-        updated_at TEXT NOT NULL
-      ) '''
-    );
+        name TEXT NOT NULL,
+        picture TEXT
+      )
+    ''');
+    batch.execute('''
+      CREATE TABLE log(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        input_date TEXT NOT NULL,
+        pay_date TEXT NOT NULL,
+        name TEXT,
+        price INTEGER NOT NULL,
+        category INTEGER,
+        picture TEXT,
+        sign INTEGER NOT NULL,
+        split_count INTEGER NOT NULL,
+        FOREIGN KEY(category) REFERENCES category(id) ON DELETE SET NULL
+      )
+    ''');
+    batch.execute('''
+      CREATE TABLE budget(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        sign INTEGER,
+        price INTEGER
+      )
+    ''');
+    batch.execute('''
+      CREATE TABLE shortcut(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        price INTEGER NOT NULL,
+        category INTEGER,
+        sign INTEGER NOT NULL
+      )
+    ''');
+    batch.execute('''
+      CREATE TABLE wish(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        price INTEGER,
+        url TEXT,
+        picture TEXT
+      )
+    ''');
+    batch.execute('''
+      CREATE TABLE notification(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        content TEXT,
+        picture TEXT
+      )
+    ''');
+    batch.execute('''
+      INSERT INTO category VALUES(1, '食費', '%')
+    ''');
   }
 }
